5ca636f7471bb813ffda01b4521637ff6231b9b2
王大爷 <wangdaye@wangdayedeMacBook-Pro.local>
2022-03-05 13:42:39 +0800

    add podfile

--- GeometricWeather/Update/Weather/WeatherKeywords.swift
+++ GeometricWeather/Update/Weather/WeatherKeywords.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-let ACCU_WEATHER_KEY = "srRLeAmTroxPinDG8Aus3Ikl6tLGJd94"
-let ACCU_CURRENT_KEY = "131778526309453295c9ce2350a79e87"
-let ACCU_AQI_KEY = "7f8c4da3ce9849ffb2134f075201c45a"
+let ACCU_WEATHER_KEY = ""
+let ACCU_CURRENT_KEY = ""
+let ACCU_AQI_KEY = ""
 let ACCU_WEATHER_BASE_URL = "https://api.accuweather.com/"
 
-let CAIYUN_WEATHER_API_KEY = "mCsnasNJUFP1-mPe"
+let CAIYUN_WEATHER_API_KEY = ""
 let CAIYUN_WEATHER_BASE_URL = "https://api.caiyunapp.com/v2.5/"
